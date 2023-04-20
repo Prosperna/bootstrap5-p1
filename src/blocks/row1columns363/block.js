fc4e8a88213f2573bec25d44ab616f47fba21303
chrismelvin <chrismelvin.04@gmail.com>
2023-04-20 17:45:30 +0800

    added author to package.json

--- src/blocks/row1columns363/block.js
+++ src/blocks/row1columns363/block.js
@@ -15,10 +15,11 @@ export default (opts = {}, blockProps) => {
     id: "1row ",
     label: "1 Row 3/6/3 Columns",
     media: `<svg class="custom-blocks" width="113" height="66" viewBox="0 0 113 66" fill="none" xmlns="http://www.w3.org/2000/svg">
-      <path d="M1 5C1 2.79086 2.79086 1 5 1H19V64H5C2.79086 64 1 62.2091 1 60V5Z" fill="#F42F1F"/>
-      <rect x="-1" y="1" width="15.6" height="64" rx="3" transform="matrix(-1 0 0 1 110.647 0)"   stroke-width="2"/>
-      <rect x="-1" y="1" width="91.1765" height="64" rx="3" transform="matrix(-1 0 0 1 92.0117 0)"   stroke-width="2"/>
-      </svg>
+    <path d="M1 5C1 2.79086 2.79086 1 5 1H13V64H5C2.79086 64 1 62.2091 1 60V5Z" fill="#FFBF00"/>
+    <rect x="1.35254" y="1" width="24.4" height="64" rx="3" stroke="#5C5C5C" stroke-width="2"/>
+    <rect x="90.3887" y="1" width="20.7765" height="64" rx="3" stroke="#5C5C5C" stroke-width="2"/>
+    <rect x="28.7881" y="1" width="58.5647" height="64" rx="3" stroke="#5C5C5C" stroke-width="2"/>
+    </svg>
               `,
     content: `<div ${attrsRow} class="gjs-row" >
     <div ${attrsCell} class="gjs-cell col-3 position-relative"><div class="layout-placeholder"></div></div>
