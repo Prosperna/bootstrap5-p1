21aedb5033472f1e3e32e7846e5f2a4f93406edb
chrismelvin <chrismelvin.04@gmail.com>
2023-04-24 09:20:34 +0800

    fix: module import issues

--- src/blocks/section/index.js
+++ src/blocks/section/index.js
@@ -1,10 +1,13 @@
-import block from './block'
+import block from "./block.js";
 
+/**
+ * Add the block to the editor
+ * @param {Editor} editor - Grapes JS Editor instance
+ * @param {Object} opts - Global block options
+ * @param {Object} blockProps - Specific block options
+ */
 
 export default (editor, opts = {}, blockProps) => {
-    // Add the block to the editor
-    editor.BlockManager.add("section", block(opts, blockProps));
-  
-
-  };
-  
+  // Add the block to the editor
+  editor.BlockManager.add("section", block(opts, blockProps));
+};
